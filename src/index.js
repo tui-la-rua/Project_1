5e948d6c6fa0c8b932730d78ad840a74a1ae9353
tui-la-rua <ruaremake@gmail.com>
2024-07-18 23:54:32 +0700

    18/07/2024 sua doi

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const { engine } = require('express-handlebars');
+const path = require('path')
 const app = express()
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 
-app.get('/a', function (req, res) {
-  res.send('Hello World')
+// cấu hình file tĩnh
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.engine('handlebars', engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'resources', 'views'))
+app.use(express.static('public')) 
+
+
+app.use(morgan('dev'))
+
+
+app.get('/home', function (req, res) {
+  res.render('home')
 })
 
 app.listen(3000)
